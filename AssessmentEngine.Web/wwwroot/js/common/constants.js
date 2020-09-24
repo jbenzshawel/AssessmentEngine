ea2bced031bfcca215b209f7bd233b95b6669003
Addison Benzshawel <jbenzshawel@gmail.com>
2020-09-24 00:12:46 -0500

    EFT UI proof of concept

--- AssessmentEngine.Web/wwwroot/js/common/constants.js
+++ AssessmentEngine.Web/wwwroot/js/common/constants.js
@@ -2,6 +2,10 @@ const Constants = {
     eftImages: {
         introScreen: '/img/eft/Intro.jpg',
         instructions: '/img/eft/Enhance_NoLoad_Instructions.jpg',
+        three: '/img/eft/3.jpg',
+        two: '/img/eft/2.jpg',
+        one: '/img/eft/1.jpg',
+        fixationCross: '/img/eft/Fix_Cross.jpg',
         // TODO: add all images
         endScreen: '/img/eft/EndScreen.jpg'
     }
---
+++ AssessmentEngine.Web/wwwroot/js/eftTask.js
@@ -0,0 +1,91 @@
+const EFTTask = function (viewModel) {
+    $(function () {
+        $(document).on('keypress', e => {
+            if (e.which === 13) { // enter
+                eft.participantTrigger();
+            }
+        });
+    })
+
+    setTimeout(BootstrapUtility.toggleLoadingSpinner, 150);
+
+    const instructionImages = [
+        Constants.eftImages.introScreen,
+        Constants.eftImages.instructions,
+        Constants.eftImages.endScreen
+    ];
+
+    const countDownImages = [
+        Constants.eftImages.three,
+        Constants.eftImages.two,
+        Constants.eftImages.one,
+        
+    ];
+
+    const eft = new Vue({
+        el: '#eftView',
+        data: function () {
+            return {
+                step: 0,
+                currentImageSrc: instructionImages[0],
+                settings: viewModel.settings
+            }
+        },
+        methods: {
+            participantTrigger: function () {
+                this.step = this.step + 1;
+                if (this.step < 2) {
+                    this.currentImageSrc = this.getInstructionStepImage();
+                } else {
+                    this.startTask();
+                }
+            },
+            getInstructionStepImage: function () {
+                const index = this.step < instructionImages.length
+                    ? this.step
+                    : instructionImages.length - 1;
+
+                return instructionImages[index];
+            },
+            startTask: function () {
+                // TODO(AB): refactor to use promises
+                
+                const base = this;
+                let countDownIndex = 0;
+                const countDownCallBack = function () {
+                    base.currentImageSrc = countDownImages[countDownIndex];
+                    countDownIndex++;
+                    
+                    if (countDownIndex === 3){
+                        window.clearInterval(countDownID);
+                        startFixationCross();
+                    }
+                };
+                
+                const countDownID = setInterval(countDownCallBack, 1000);
+                
+                const startFixationCross = function() {
+                    let fixationIndex = 0;
+                    const fixationCallback = function() {
+                        if (fixationIndex === 0) {
+                            base.currentImageSrc = Constants.eftImages.fixationCross
+                        }
+                        
+                        fixationIndex++;
+                        
+                        if (fixationIndex === 4) {
+                            window.clearInterval(fixationID)
+                        }
+                    };
+                    
+                    const fixationID = setInterval( fixationCallback,1000);
+                };
+                
+            }
+        },
+    });
+
+    return {
+        eft: eft
+    }
+}
