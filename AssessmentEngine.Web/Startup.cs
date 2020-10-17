e9f35c02dbc2ad5de9139469276ac794e36eedb3
Addison Benzshawel <jbenzshawel@gmail.com>
2020-10-17 14:23:21 -0500

    Reverse proxy config

--- AssessmentEngine.Web/Startup.cs
+++ AssessmentEngine.Web/Startup.cs
@@ -6,6 +6,7 @@ using AssessmentEngine.Web.Areas.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -74,7 +75,13 @@ namespace AssessmentEngine.Web
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts(); // todo: Uncomment once https configured on server
+                
+                // Used for nginx reverse proxy
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
             }
 
             app.UseHttpsRedirection();
