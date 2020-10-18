e5d0b7fc18a19bc4619e1c3aab495a9366a22053
Addison Benzshawel <jbenzshawel@gmail.com>
2020-10-18 11:07:18 -0500

    Style updates

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Building app..."
+yarn --cwd AssessmentEngine.Web/wwwroot/ install
+gulp --gulpfile AssessmentEngine.Web/wwwroot/gulpfile.js
 dotnet publish --configuration Release
 
 echo "Stopping assessment service..."
@@ -7,7 +9,7 @@ systemctl stop assessment.service
 
 echo "Publishing app..."
 rm -r /var/www/assessment-engine/*
-cp AssessmentEngine.Web/bin/Release/netcoreapp3.1/publish/* /var/www/assessment-engine/.
+cp -r AssessmentEngine.Web/bin/Release/netcoreapp3.1/publish/* /var/www/assessment-engine/.
 chown -R assessment:assessment /var/www/assessment-engine/*
 
 echo "Starting assessment service..."
